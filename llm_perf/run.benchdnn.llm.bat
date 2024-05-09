89c35acad663276d12f32160060d72035abb5908
Kim, SungEun <sungeun.kim@intel.com>
2024-05-09 08:48:51 +0000

    add benchdnn scripts for llm

---
+++ llm_perf/run.benchdnn.llm.bat
@@ -0,0 +1,42 @@
+echo on
+@REM call chatglm3.in_1084.1st.txt.run.sh.cmd > chatglm3.in_1084.1st.txt.run.sh.cmd.txt
+@REM call chatglm3.in_1084.2nd.txt.run.sh.cmd > chatglm3.in_1084.2nd.txt.run.sh.cmd.txt
+@REM call chatglm3.in_20.1st.txt.run.sh.cmd > chatglm3.in_20.1st.txt.run.sh.cmd.txt
+@REM call chatglm3.in_20.2nd.txt.run.sh.cmd > chatglm3.in_20.2nd.txt.run.sh.cmd.txt
+@REM call chatglm3.in_290.1st.txt.run.sh.cmd > chatglm3.in_290.1st.txt.run.sh.cmd.txt
+@REM call chatglm3.in_290.2nd.txt.run.sh.cmd > chatglm3.in_290.2nd.txt.run.sh.cmd.txt
+@REM call chatglm3.in_3083.1st.txt.run.sh.cmd > chatglm3.in_3083.1st.txt.run.sh.cmd.txt
+@REM call chatglm3.in_3083.2nd.txt.run.sh.cmd > chatglm3.in_3083.2nd.txt.run.sh.cmd.txt
+@REM call chatglm3.in_539.1st.txt.run.sh.cmd > chatglm3.in_539.1st.txt.run.sh.cmd.txt
+@REM call chatglm3.in_539.2nd.txt.run.sh.cmd > chatglm3.in_539.2nd.txt.run.sh.cmd.txt
+@REM call qwen.in_1052.1st.txt.run.sh.cmd > qwen.in_1052.1st.txt.run.sh.cmd.txt
+@REM call qwen.in_1052.2nd.txt.run.sh.cmd > qwen.in_1052.2nd.txt.run.sh.cmd.txt
+@REM call qwen.in_2048.1st.txt.run.sh.cmd > qwen.in_2048.1st.txt.run.sh.cmd.txt
+@REM call qwen.in_2048.2nd.txt.run.sh.cmd > qwen.in_2048.2nd.txt.run.sh.cmd.txt
+@REM call qwen.in_25.1st.txt.run.sh.cmd > qwen.in_25.1st.txt.run.sh.cmd.txt
+@REM call qwen.in_25.2nd.txt.run.sh.cmd > qwen.in_25.2nd.txt.run.sh.cmd.txt
+@REM call qwen.in_280.1st.txt.run.sh.cmd > qwen.in_280.1st.txt.run.sh.cmd.txt
+@REM call qwen.in_280.2nd.txt.run.sh.cmd > qwen.in_280.2nd.txt.run.sh.cmd.txt
+@REM call qwen.in_538.1st.txt.run.sh.cmd > qwen.in_538.1st.txt.run.sh.cmd.txt
+@REM call qwen.in_538.2nd.txt.run.sh.cmd > qwen.in_538.2nd.txt.run.sh.cmd.txt
+
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py chatglm3.in_1084.1st.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py chatglm3.in_1084.2nd.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py chatglm3.in_20.1st.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py chatglm3.in_20.2nd.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py chatglm3.in_290.1st.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py chatglm3.in_290.2nd.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py chatglm3.in_3083.1st.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py chatglm3.in_3083.2nd.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py chatglm3.in_539.1st.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py chatglm3.in_539.2nd.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py qwen.in_1052.1st.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py qwen.in_1052.2nd.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py qwen.in_2048.1st.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py qwen.in_2048.2nd.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py qwen.in_25.1st.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py qwen.in_25.2nd.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py qwen.in_280.1st.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py qwen.in_280.2nd.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py qwen.in_538.1st.txt.run.sh.cmd.txt
+python c:\dev\sungeunk\libraries.ai.videoanalyticssuite.gpu-tools\format_benchdnn.py qwen.in_538.2nd.txt.run.sh.cmd.txt
